943bbb294f292efbf41ca54a853ff213a37fad23
Guilherme Oliveira <guibalbinos@gmail.com>
2025-04-23 17:42:21 -0300

    status_botão

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector(`dashboard__item__img`);
+    let botao = jogoClicado.querySelector(`dashboard__item__button`);
+}
